cb38da66fcabecab67ad65aa3240f03e85102391
Div072 <pdivyesh072@gmail.com>
2024-10-22 21:41:57 +0530

    Basic memory allocator in C

--- example.c
+++ example.c
@@ -7,7 +7,6 @@ int main() {
     int *p1 = (int*)my_malloc(sizeof(int)*10);
 
     if(p1) {
-        printf("Allocated 100 bytes at %p\n", p1);
         for(int i=0;i<10;i++) {
             p1[i] = i;
             printf("Element at p[%d]: %d\n",p1[i],i);
@@ -17,12 +16,8 @@ int main() {
     int *p2 = (int*)my_malloc(sizeof(int)*20);
 
 
-    if(p2) {
-        printf("Allocated 1024*1024 bytes at %p\n", p2);
-    }
     // Free memory and try re-allocating
     p1 = my_free(p1);
-
     my_realloac(0,p2);
 
     // Clean up and release the memory pool
